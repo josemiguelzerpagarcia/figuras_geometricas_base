b5ae11e1a53986f73bdafbd15cfd5cb3047810d1
josemiguelzerpagarcia <zerpa.garcia.jose.miguel@iescamas.es>
2025-02-04 22:05:17 +0100

    cuadrado añadido

---
+++ code/src/main/java/com/endes/figuras/Cuadrado.java
@@ -0,0 +1,39 @@
+package com.endes.figuras;
+
+
+public class Cuadrado extends com.endes.figuras.FiguraGeometrica{
+
+	protected Punto esquina;
+	protected double lado;
+	
+	public Cuadrado(String nombre, String color, Punto esquina, double lado) {
+		super(nombre, color);
+		this.esquina = esquina;
+		if (lado < 0) {
+			this.lado = 1;
+		} else {
+			this.lado = lado;
+		}
+		
+	}
+	
+	@Override
+	public double area() {
+		double area;
+		area = lado * lado;
+		return area;
+	}
+
+	@Override
+	public double perimetro() {
+		double perimetro;
+		perimetro = lado * 4;
+		return perimetro;
+	}
+	
+	
+	public double getLado() {
+		return lado;
+	}
+	
+}
